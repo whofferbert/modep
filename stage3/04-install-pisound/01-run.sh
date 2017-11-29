a7ccfd8f89cad8157f6b7d8b501afb177e60b06e
Giedrius Trainavicius <giedrius@blokas.io>
2017-11-29 03:41:12 +0200

    Enable Pisound's WiFi hotspot by default in the image

--- stage3/04-install-pisound/01-run.sh
+++ stage3/04-install-pisound/01-run.sh
@@ -1,8 +1,11 @@
 git clone https://github.com/BlokasLabs/modep-btn-scripts.git ${ROOTFS_DIR}/usr/local/modep/modep-btn-scripts
 
 cp files/pisound.conf ${ROOTFS_DIR}/etc/
+install -m 644 files/*.service ${ROOTFS_DIR}/usr/lib/systemd/system
 
 on_chroot << EOF
 	apt-get remove -y python-openssl # Improves 'import requests' load time
+	systemctl daemon-reload
+	systemctl enable pisound-hotspot
 EOF
 
