8b28cfa0402b7a181371133444ce0037e427577e
Giedrius Trainavicius <giedrius@blokas.io>
2017-12-13 04:19:32 +0200

    A fix for setting the hostname

--- stage3/01-misc-config/00-run.sh
+++ stage3/01-misc-config/00-run.sh
@@ -2,6 +2,6 @@
 touch ${ROOTFS_DIR}/boot/ssh
 
 # Set the new hostname.
-CURRENT_HOSTNAME=`cat /etc/hostname | tr -d " \t\n\r"`
+CURRENT_HOSTNAME=`cat ${ROOTFS_DIR}/etc/hostname | tr -d " \t\n\r"`
 echo $HOSTNAME > ${ROOTFS_DIR}/etc/hostname
-sed -i "s/127.0.1.1.*$CURRENT_HOSTNAME/127.0.1.1\t$HOSTNAME/g" ${ROOTFS_DIR}/etc/hosts
+sed -i 's/127.0.1.1.*'"$CURRENT_HOSTNAME"'/127.0.1.1\t'"$HOSTNAME"'/g' ${ROOTFS_DIR}/etc/hosts
