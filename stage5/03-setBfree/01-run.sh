672881e42983e84b58fa5e8513a44243b3984ce5
Giedrius Trainavicius <giedrius@blokas.io>
2017-12-25 00:50:14 +0200

    Added setBfree plugins

---
+++ stage5/03-setBfree/01-run.sh
@@ -0,0 +1,16 @@
+PLUGIN=setBfree
+GIT_URI="https://github.com/BlokasLabs/setBfree"
+TMP_DIR=${ROOTFS_DIR}/tmp/${PLUGIN}
+
+rm -rf ${TMP_DIR}
+git clone --depth 1 ${GIT_URI} ${TMP_DIR}
+pushd ${TMP_DIR}
+
+CC=arm-linux-gnueabihf-gcc make MOD=1 OPTIMIZATIONS="-ffast-math -fomit-frame-pointer -O3 -fno-finite-math-only"
+
+CC=arm-linux-gnueabihf-gcc make install MOD=1 DESTDIR=${ROOTFS_DIR} lv2dir=${LV2_DIR}
+
+popd
+
+rm -rf ${TMP_DIR}
+
