dedd1259a36aebc00041dfe6cc1fb0b83daaeb78
Giedrius <giedrius@blokas.io>
2018-01-03 00:22:47 +0200

    Added fomp plugin

---
+++ stage5/06-fomp/01-run.sh
@@ -0,0 +1,19 @@
+PLUGIN=fomp.lv2
+GIT_URI=https://github.com/BlokasLabs/${PLUGIN}.git
+TMP_DIR=${ROOTFS_DIR}/tmp/${PLUGIN}
+
+export CC=arm-linux-gnueabihf-gcc
+export CXX=arm-linux-gnueabihf-g++
+export LD=arm-linux-gnueabihf-ld
+export STRIP=arm-linux-gnueabihf-strip
+
+rm -rf ${TMP_DIR}
+git clone --depth 1 ${GIT_URI} ${TMP_DIR}
+pushd ${TMP_DIR}
+./waf configure --lv2dir=${LV2_ABS_DIR}
+./waf build -j4
+./waf -j1 install
+popd
+
+rm -r ${TMP_DIR}
+
