99d2b4954596914500838e3be44840340e385f3c
Giedrius <giedrius@blokas.io>
2018-01-08 00:48:58 +0200

    Added sooperlooper plugin

---
+++ stage5/12-sooperlooper/01-run.sh
@@ -0,0 +1,20 @@
+PLUGIN=sooperlooper.lv2
+GIT_URI="https://github.com/BlokasLabs/sooperlooper-lv2-plugin"
+TMP_DIR=${ROOTFS_DIR}/tmp/${PLUGIN}
+
+rm -rf ${TMP_DIR}
+git clone --depth 1 ${GIT_URI} ${TMP_DIR}
+pushd ${TMP_DIR}/sooperlooper
+
+export CC=arm-linux-gnueabihf-gcc
+export CXX=arm-linux-gnueabihf-g++
+export LD=arm-linux-gnueabihf-gcc
+export STRIP=arm-linux-gnueabihf-strip
+
+CFLAGS="--sysroot=${ROOTFS_DIR}" LDFLAGS="--sysroot=${ROOTFS_DIR}" make VERBOSE=1 -j4
+make install INSTALLATION_PATH=${LV2_ABS_DIR}/${PLUGIN}
+
+popd
+
+rm -rf ${TMP_DIR}
+
