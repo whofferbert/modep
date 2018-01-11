1611f5bb4ff1fecdaea4d803a250da8adcb79999
Giedrius <giedrius@blokas.io>
2018-01-11 16:40:04 +0200

    Adding triceratops.lv2 plugin

---
+++ stage5/27-triceratops/01-run.sh
@@ -0,0 +1,22 @@
+PLUGIN=triceratops.lv2
+GIT_URI=https://github.com/BlokasLabs/${PLUGIN}.git
+TMP_DIR=${ROOTFS_DIR}/tmp/${PLUGIN}
+
+export CC=arm-linux-gnueabihf-gcc
+export CXX=arm-linux-gnueabihf-g++
+export LD=arm-linux-gnueabihf-ld
+export STRIP=arm-linux-gnueabihf-strip
+
+export CXXFLAGS="--sysroot=${ROOTFS_DIR} -fPIC"
+export LDFLAGS="--sysroot=${ROOTFS_DIR} -fPIC"
+
+rm -rf ${TMP_DIR}
+git clone --depth 1 ${GIT_URI} ${TMP_DIR}
+pushd ${TMP_DIR}
+./waf configure --prefix=/usr/ --destdir=${ROOTFS_DIR} --lv2dir=${LV2_ABS_DIR}
+./waf build -j4 -v
+./waf -j1 install
+popd
+
+rm -r ${TMP_DIR}
+
