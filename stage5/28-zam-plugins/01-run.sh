a6ba20c89826a61d67092f7d348df6ed21d06e46
Giedrius <giedrius@blokas.io>
2018-01-11 18:22:11 +0200

    Adding Zam plugins

---
+++ stage5/28-zam-plugins/01-run.sh
@@ -0,0 +1,32 @@
+PLUGIN=zam-plugins
+GIT_URI="https://github.com/BlokasLabs/${PLUGIN}"
+TMP_DIR=${ROOTFS_DIR}/tmp/${PLUGIN}
+
+rm -rf ${TMP_DIR}
+git clone --depth 1 --recursive ${GIT_URI} ${TMP_DIR}
+pushd ${TMP_DIR}
+
+export CC=arm-linux-gnueabihf-gcc
+export CXX=arm-linux-gnueabihf-g++
+export LD=arm-linux-gnueabihf-gcc
+export STRIP=arm-linux-gnueabihf-strip
+
+NOOPT=true CFLAGS="--sysroot=${ROOTFS_DIR}" LDFLAGS="--sysroot=${ROOTFS_DIR}" HAVE_DGL=false make VERBOSE=1 -j4 V=1
+
+on_chroot << EOF
+	cd /tmp/${PLUGIN}
+	/tmp/${PLUGIN}/dpf/utils/generate-ttl.sh
+EOF
+
+#make NOOPT=true HAVE_DGL=false install DESTDIR=${ROOTFS_DIR} PREFIX=/tmp/${PLUGIN} INSTALL_SYS_PLUGINS_DIR=${LV2_DIR} VERBOSE=1 V=1
+
+PLUGINS_TO_INSTALL="ZamAutoSat.lv2 ZaMaximX2.lv2 ZamComp.lv2 ZamCompX2.lv2 ZamDelay.lv2 ZamEQ2.lv2 ZamGate.lv2 ZamGateX2.lv2 ZamGEQ31.lv2 ZamHeadX2.lv2 ZamTube.lv2 ZaMultiComp.lv2 ZaMultiCompX2.lv2"
+
+for p in ${PLUGINS_TO_INSTALL}; do
+	mv ${TMP_DIR}/bin/${p} ${LV2_ABS_DIR}/
+done
+
+popd
+
+rm -r ${TMP_DIR}
+
