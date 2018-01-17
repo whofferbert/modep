9fd0ed2556db57dc437efa1c2731e59c7e97e906
Giedrius <giedrius@blokas.io>
2018-01-17 16:32:34 +0200

    Copy more of the lv2 plugin data where available

--- stage5/99-copy-modguis/01-run.sh
+++ stage5/99-copy-modguis/01-run.sh
@@ -7,5 +7,14 @@ for i in `comm -12 <(ls ${DATA_DIR}/plugins-fixed | grep -v bad) <(ls ${LV2_ABS_
 	cp -r ${DATA_DIR}/plugins-fixed/$i ${LV2_ABS_DIR}/
 done
 
+for i in `comm -12 <(ls ${DATA_DIR}/plugins | grep -v bad) <(ls ${LV2_ABS_DIR})`; do
+	if test -e ${DATA_DIR}/plugins/$i/modgui; then
+		echo Copying $i data...;
+		cp -r ${DATA_DIR}/plugins/$i ${LV2_ABS_DIR}/
+	else
+		echo Skipping $i with no modgui folder...
+	fi
+done
+
 rm -rf ${DATA_DIR}
 
