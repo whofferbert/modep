ccdc1771d0684c403398465dc640d0c3bc91a159
Ghislain Bourgeois <ghislain.bourgeois@gmail.com>
2018-03-06 07:39:01 -0500

    Fix compilation of some plugins inside chroot

--- stage5/05-infamousPlugins/01-run-chroot.sh
+++ stage5/05-infamousPlugins/01-run-chroot.sh
@@ -4,6 +4,20 @@ PLUGIN=infamousPlugins
 GIT_URI="https://github.com/BlokasLabs/${PLUGIN} -b gui"
 TMP_DIR=/tmp/${PLUGIN}
 
+CMAKE_TOOLCHAIN=$(mktemp /tmp/cmake.toolchain.XXXXXXX)
+cat << EOT > $CMAKE_TOOLCHAIN
+    SET(CMAKE_SYSTEM_NAME Linux)
+    SET(CMAKE_SYSTEM_VERSION 1)
+
+    SET(CMAKE_C_COMPILER   /usr/bin/arm-linux-gnueabihf-gcc)
+    SET(CMAKE_CXX_COMPILER /usr/bin/arm-linux-gnueabihf-g++)
+
+    SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+    SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+    SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+EOT
+export CMAKE_TOOLCHAIN
+
 rm -rf ${TMP_DIR}
 git clone --depth 1 ${GIT_URI} ${TMP_DIR}
 pushd ${TMP_DIR}
--- stage5/11-rkrlv2/01-run-chroot.sh
+++ stage5/11-rkrlv2/01-run-chroot.sh
@@ -8,6 +8,20 @@ TMP_DIR=/tmp/${PLUGIN}
 #unset PKG_CONFIG_LIBDIR
 #unset PKG_CONFIG_SYSROOT_DIR
 
+CMAKE_TOOLCHAIN=$(mktemp /tmp/cmake.toolchain.XXXXXXX)
+cat << EOT > $CMAKE_TOOLCHAIN
+    SET(CMAKE_SYSTEM_NAME Linux)
+    SET(CMAKE_SYSTEM_VERSION 1)
+
+    SET(CMAKE_C_COMPILER   /usr/bin/arm-linux-gnueabihf-gcc)
+    SET(CMAKE_CXX_COMPILER /usr/bin/arm-linux-gnueabihf-g++)
+
+    SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+    SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+    SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+EOT
+export CMAKE_TOOLCHAIN
+
 rm -rf ${TMP_DIR}
 git clone --depth 1 ${GIT_URI} ${TMP_DIR}
 pushd ${TMP_DIR}
