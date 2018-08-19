610ba0be963563c51da5832c836c6c9d94b02bff
Giedrius <giedrius@blokas.io>
2018-08-20 01:06:16 +0300

    * Pisound hotspot name renamed to MODEP * Pisound App integration for switching pedalboards * Have jackd-1 tweak RT priorities * Use 'performance' cpu scaling governor by default

--- stage3/04-install-pisound/01-run.sh
+++ stage3/04-install-pisound/01-run.sh
@@ -10,5 +10,14 @@ on_chroot << EOF
 	apt-get remove -y python-openssl # Improves 'import requests' load time
 	systemctl daemon-reload
 	systemctl enable pisound-hotspot
+	systemctl enable cpu_performance_scaling_governor
+	systemctl disable raspi-config # raspi-config is only enabling 'ondemand' governor as of 2018.08.19
 EOF
+
+rm -rf ${ROOTFS_DIR}/usr/local/pisound-ctl/modep
+git clone https://github.com/BlokasLabs/modep-ctl-scripts.git ${ROOTFS_DIR}/usr/local/pisound-ctl/modep
+echo echo system/system.yml > ${ROOTFS_DIR}/usr/local/pisound-ctl/category_list.sh
+echo echo modep/modep.yml >> ${ROOTFS_DIR}/usr/local/pisound-ctl/category_list.sh
+
+cp files/hostapd.conf ${ROOTFS_DIR}/usr/local/pisound/scripts/pisound-btn/hostapd.conf
 
