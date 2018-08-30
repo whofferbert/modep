771687baa2f5c3241bd654135e12bac40a09eda3
Giedrius <giedrius@blokas.io>
2018-08-31 01:14:08 +0300

    Rotate logs more often than daily, to avoid running out of disk space

--- stage3/01-misc-config/00-run.sh
+++ stage3/01-misc-config/00-run.sh
@@ -7,3 +7,8 @@ touch ${ROOTFS_DIR}/boot/ssh
 CURRENT_HOSTNAME=`cat ${ROOTFS_DIR}/etc/hostname | tr -d " \t\n\r"`
 echo $HOSTNAME > ${ROOTFS_DIR}/etc/hostname
 sed -i 's/127.0.1.1.*'"$CURRENT_HOSTNAME"'/127.0.1.1\t'"$HOSTNAME"'/g' ${ROOTFS_DIR}/etc/hosts
+
+sed -i 's/weekly/size 1M/g' ${ROOTFS_DIR}/etc/logrotate.d/rsyslog
+sed -i 's/daily/size 1M/g' ${ROOTFS_DIR}/etc/logrotate.d/rsyslog
+
+mv ${ROOTFS_DIR}/etc/cron.daily/logrotate ${ROOTFS_DIR}/etc/cron.hourly/
